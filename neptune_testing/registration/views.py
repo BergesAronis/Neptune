5b49b70901945e32246e701d904e84871c46a2e7
Berges <iraniber@hotmail.com>
2018-02-28 08:40:08 -0500

    Detailed user registration

--- neptune_testing/registration/views.py
+++ neptune_testing/registration/views.py
@@ -2,17 +2,19 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 
+from . import forms
+
 
 def index(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = forms.RegistrationForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
-            return redirect('admin')
+            return redirect('/admin')
     else:
-        form = UserCreationForm()
+        form = forms.RegistrationForm()
     return render(request, 'registration/index.html', {'form': form})
