79115108445b5fc3a362aecc41394e62baba070a
Berges <iraniber@hotmail.com>
2018-03-24 15:31:54 -0400

    Created user registration functionality

--- neptune_testing/registration/views.py
+++ neptune_testing/registration/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 
 from . import forms
@@ -9,8 +9,11 @@ def index(request):
     if request.method == 'POST':
         form = forms.RegistrationForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
+            user.refresh_from_db()
             username = form.cleaned_data.get('username')
+            user.profile.birth_date = form.cleaned_data.get('birth_date')
+            user.save()
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
