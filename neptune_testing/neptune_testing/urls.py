110c6a61f0f96e3a20e47abb1ca0508e3b163c5f
carterspearson <carterspearson@gmail.com>
2018-05-29 22:20:52 -0400

    Switching branches

--- neptune_testing/neptune_testing/urls.py
+++ neptune_testing/neptune_testing/urls.py
@@ -13,10 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^logout/$', auth_views.logout, name='logout'),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
