5d3dc25a26b833d900525cf2463698367d49b56b
carterspearson <carterspearson@gmail.com>
2018-05-30 21:43:50 -0400

    Used startapp to create login app

--- neptune_testing/login/apps.py
+++ neptune_testing/login/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class RegistrationConfig(AppConfig):
-    name = 'registration'
+class LoginConfig(AppConfig):
+    name = 'login'
--- neptune_testing/login/views.py
+++ neptune_testing/login/views.py
@@ -1,23 +1,10 @@
-from django.contrib.auth import login, authenticate
-from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render
+import datetime
 
-from . import forms
+from registration import forms
 
+# Create your views here.
 
 def index(request):
-    if request.method == 'POST':
-        form = forms.RegistrationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            user.refresh_from_db()
-            username = form.cleaned_data.get('username')
-            user.profile.birth_date = form.cleaned_data.get('birth_date')
-            user.save()
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
-            login(request, user)
-            return redirect('/admin')
-    else:
-        form = forms.RegistrationForm()
+    form = forms.RegistrationForm()
     return render(request, 'registration/index.html', {'form': form})
