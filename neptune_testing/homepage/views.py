df7cafbee4bb853b8a0495bcc639c5357972642a
Berges <iraniber@hotmail.com>
2018-06-04 22:08:48 -0400

    Added inline logout feature

--- neptune_testing/homepage/views.py
+++ neptune_testing/homepage/views.py
@@ -1,21 +1,21 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.forms import UserCreationForm
 
-from account_handling import forms
 
 def index(request):
     if request.method == 'POST':
-        form = forms.LoginForm(request.POST)
-        if form.is_valid():
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password')
+        if 'logout' in request.POST:
+            print('logout')
+            logout(request)
+            return redirect('/')
+        elif 'login' in request.POST:
+            username = request.POST.get("username", "")
+            raw_password = request.POST.get("password", "")
             user = authenticate(request, username=username, password=raw_password)
             if user is not None:
                 login(request, user)
                 return redirect('/')
             else:
-                return render(request, 'homepage/index.html', {'form': form})
-    else:
-        form = forms.LoginForm()
+                return render(request, 'homepage/index.html')
     return render(request, 'homepage/index.html')
