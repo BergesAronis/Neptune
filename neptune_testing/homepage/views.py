8a2f70697816ea2f0d991ebfbc55900c7db37013
Berges <iraniber@hotmail.com>
2018-06-04 21:54:16 -0400

    Connected login submission on homepage

--- neptune_testing/homepage/views.py
+++ neptune_testing/homepage/views.py
@@ -1,4 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+
+from account_handling import forms
 
 def index(request):
+    if request.method == 'POST':
+        form = forms.LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=raw_password)
+            if user is not None:
+                login(request, user)
+                return redirect('/')
+            else:
+                return render(request, 'homepage/index.html', {'form': form})
+    else:
+        form = forms.LoginForm()
     return render(request, 'homepage/index.html')
