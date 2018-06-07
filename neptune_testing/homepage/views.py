addd88676c5a16b0c125b23659cf6d846263e6c9
Berges <iraniber@hotmail.com>
2018-06-06 21:34:18 -0400

    Inserted sign up form into bootstrap modal

--- neptune_testing/homepage/views.py
+++ neptune_testing/homepage/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.forms import UserCreationForm
 
+from account_handling import forms
+
 
 def index(request):
-    return render(request, 'homepage/index.html')
+    form = forms.RegistrationForm()
+    return render(request, 'homepage/index.html', {'form': form,})
