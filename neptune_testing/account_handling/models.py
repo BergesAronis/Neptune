422b3987132ff014b5eb8c33f6899c60b85e044c
Berges <iraniber@hotmail.com>
2018-06-13 21:25:36 -0400

    Removed UserType class in abstraction

--- neptune_testing/account_handling/models.py
+++ neptune_testing/account_handling/models.py
@@ -7,6 +7,9 @@ from django.dispatch import receiver
 class UserType(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
+    class Meta:
+        abstract = True
+
     def __str__(self):
         return "undefined user"
 
@@ -17,11 +20,13 @@ class Agent(UserType):
     def __str__(self):
         return "Agent"
 
+
 class Staff(UserType):
     company = models.CharField(max_length=100, blank=True, null=True)
 
     def __str__(self):
         return "Staff"
+
 
 class Client(UserType):
     agent_company = models.CharField(max_length=100, blank=True, null=True)
