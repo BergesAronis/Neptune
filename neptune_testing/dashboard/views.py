f5e5627426b106d3feb5a722867b772b93c4ca7e
carterspearson <carterspearson@gmail.com>
2018-06-24 20:04:22 -0400

    Dashboard loads navigation apps from database

--- neptune_testing/dashboard/views.py
+++ neptune_testing/dashboard/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
+from .models import Navigation
 
 
 def index(request):
-    return render(request, 'dashboard/dashboard.html')
+    navigation_apps = Navigation.objects.values()
+    return render(request, 'dashboard/dashboard.html',  {'navigation_apps': navigation_apps})
